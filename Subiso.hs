246b6e60ec9a7267556636c215a6d2f182a3af96
Cai Yufei <0.yfcai@gmail.com>
2011-08-21 13:45:02 +0800

    Fixed 2 bugs.
    1. In `membership` (formerly `membership` inside `subiso`):
       Violation of constraint (length return_value == length first_argument).
    
    2. In `trim_candidates` (formerly `f` of `new_vss` of `subiso`):
       The vertex `v` counted as a nonneighbour of itself.

--- Subiso.hs
+++ Subiso.hs
@@ -4,16 +4,16 @@ import Data.List(sort)
 
 
 endl = putStr "\n"
-main = endl >> print (subiso cyc6 cube)
+main = endl >> print (subiso _C6 cube) >> print (subiso cube _C6)
 
-cube = graph v e where
-   v = [0,1,2,3,4,5,6,7]
-   e = [(0,1), (0,2), (0,4), (1,3), (1,5), (2,3),
+cube = graph _V _E where
+ _V = [0,1,2,3,4,5,6,7]
+ _E = [(0,1), (0,2), (0,4), (1,3), (1,5), (2,3),
         (2,6), (3,7), (4,5), (4,6), (5,7), (6,7)]
 
-cyc6 = graph v e where
-   v = [0,1,2,3,4,5]
-   e = [(0,1), (1,2), (2,3), (3,4), (4,5), (5,0)]
+_C6 = graph _V _E where
+ _V = [0,1,2,3,4,5]
+ _E = [(0,1), (1,2), (2,3), (3,4), (4,5), (5,0)]
 
 
 -- Natural tree: a dictionary with members of [0, 1 ..] as key
@@ -48,6 +48,15 @@ modify f = g where
 update :: a -> Int -> (N_tree a) -> (N_tree a)
 update x = modify (const x)
 
+-- insert an entry into a sorted list
+
+insert_by :: (a -> a -> Ordering) -> a -> [a] -> [a]
+insert_by cmp = loop where
+ loop x [] = [x]
+ loop x (y:ys) = case cmp x y of
+  LT -> x : y : ys
+  _ -> y : loop x ys
+
 -- sorted list intersection
 -- identical to Data.List.Ordered.isectBy (19 Aug 2011)
 
@@ -81,16 +90,15 @@ data Vertex = V Int [Vertex]
 type Graph = [Vertex]
 
 instance Show Vertex where
- show (V i xs) = ' ' : show i ++ ": " ++
-  show (map (\(V i _) -> i) xs) ++ "\n"
+ show (V i xs) = '\n':' ':show i ++ ": " ++ show (map (\(V i _) -> i) xs)
 
 graph :: [Int] -> [(Int, Int)] -> Graph
 graph unsorted_vertices unsorted_edges = extract verts where
  verts = sort unsorted_vertices
  vtree = foldr add_vertex (n_tree Nothing) verts where
-         add_vertex i = update (Just (create i)) i
+  add_vertex i = update (Just (create i)) i
  etree = foldr add_edge (n_tree []) unsorted_edges where
-         add_edge (i, j) = modify (j :) i . modify (i :) j
+  add_edge (i, j) = modify (j :) i . modify (i :) j
  extract = map (fromJust . (vtree !))
  create i = V i (extract (sort (etree ! i)))
 
@@ -100,6 +108,30 @@ neighbourhood (V _ vs) = vs
 identifier :: Vertex -> Int
 identifier (V i _) = i
 
+gcompare :: Vertex -> Vertex -> Ordering
+gcompare (V i _) (V j _) = compare i j
+
+ginsert :: Vertex -> [Vertex] -> [Vertex]
+ginsert = insert_by gcompare
+
+gisect :: [Vertex] -> [Vertex] -> [Vertex]
+gisect = isect_by gcompare
+
+gminus :: [Vertex] -> [Vertex] -> [Vertex]
+gminus = minus_by gcompare
+
+-- outputs a boolean list the same length as the first argument
+-- indicating the membership of each element of the first list
+-- in the second list. expect both lists to be sorted.
+
+membership :: [Vertex] -> [Vertex] -> [Bool]
+membership [] _ = []
+membership xs [] = map (const False) xs
+membership (x:xs) (y:ys) = case gcompare x y of
+ LT -> False : membership xs (y:ys)
+ EQ -> True : membership xs ys
+ GT -> membership (x:xs) ys
+
 -- outputs an induced subgraph of _G isomorphic to _H if exists
 
 subiso :: Graph -> Graph -> Maybe [Vertex]
@@ -107,16 +139,11 @@ subiso _H _G = isosub _H (map (const _G) _H) where
  isosub _ ([]:_) = Nothing
  isosub _ (gs:[]) = Just [head gs]
  isosub (a:as) ((v:vs):vss) = let
-  adjacency_to_a = membership as (neighbourhood a) where
-   membership [] _ = []
-   membership _ [] = []
-   membership (x:xs) (y:ys) = case compare (identifier x) (identifier y) of
-    LT -> False : membership xs (y:ys)
-    EQ -> True : membership xs ys
-    GT -> membership (x:xs) ys
-  new_vss = zipWith f adjacency_to_a vss where
-   gcompare (V i _) (V j _) = compare i j
-   f is_neighbour vs' = if is_neighbour then
-    isect_by gcompare vs' (neighbourhood v) else
-    minus_by gcompare vs' (neighbourhood v)
+  adjacency_to_a = membership as (neighbourhood a)
+  new_vss = zipWith trim_candidates adjacency_to_a vss where
+   neibours_v = neighbourhood v
+   nonneibs_v = ginsert v neibours_v
+   trim_candidates is_neighbour candidates = if is_neighbour then
+    gisect candidates neibours_v else
+    gminus candidates nonneibs_v
   in mplus (isosub as new_vss >>= (Just . (v:))) (isosub (a:as) (vs:vss))
