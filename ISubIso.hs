e7aa3424e1f141ff85f669f0761df53572ea8fba
Cai Yufei <0.yfcai@gmail.com>
2011-08-23 12:32:02 +0800

    Great ugliness coalescing into from_file :: String -> IO (Graph, String)

--- ISubIso.hs
+++ ISubIso.hs
@@ -4,7 +4,7 @@ module ISubIso (Graph, Vertex, graph, isubiso) where
 
 import Control.Monad(mplus)
 import Data.Maybe(maybe, fromJust)
-import Data.List(sort)
+import Data.List(nub, sort)
 
 import Data.Char(isSpace)
 import Text.Parsec.String(Parser)
@@ -203,8 +203,6 @@ tokeniser = meaningless >> many token where
 
 data Data = Vertex_data {tikz_name :: Int, tikz_loc, tikz_desc :: String}
           | Edge_data {edge_generator :: String, edge_arguments :: [Int]}
-          deriving Show
--- TODO: convert 'deriving Show' of previous line to tikz code generation
 
 -- have to roll my own C programmer's natural number parser
 
@@ -234,8 +232,63 @@ annotator = mapM parse_string where
 -- the last stage: code generation, or the meaning of words
 -- without which, what would government be?
 
--- TODO: THINK ABOUT HOW TO GET RID OF THE PARSER BOX
--- SURELY WE HAVE NO NEED OF IT IN CODE GENERATION
+-- extract tikz code from vertex/edge data
+
+instance Show Data where
+ show graph_data = let
+  ref name = "(" ++ show name ++ ")"
+  draw sth = "\n\\draw" ++ sth ++ ";"
+  walk ns = if null ns then
+   [] else ref (head ns) ++ ( (tail ns) >>= ("--" ++) . ref)
+  edge_to_tikz (x, y) = ref x ++ "--" ++ ref y
+  in
+  case graph_data of
+  (Vertex_data name loc desc) ->
+   "\n\\node at" ++ loc ++ ref name ++ "{$" ++ desc ++ "$};"
+  (Edge_data generator args) ->
+   case generator of
+   -- walk and cycle has shorthands in tikz
+   -- other edges are printed as is
+   "c" -> {-cycle  -} draw (walk args ++ "--" ++ (ref . head) args)
+   "w" -> {-walk   -} draw (walk args)
+   gen -> {-default-} draw (concatMap edge_to_tikz (gen_edges gen args))
+
+-- let the generator generate edges from arguments :-O
+
+gen_edges :: String -> [Int] -> [(Int, Int)]
+gen_edges generator args = let
+ make_pair [] = []
+ make_pair (x:y:zs) = (x, y) : make_pair zs
+ walk = zip args (tail args)
+ in case generator of
+ "c" -> {-cycle  -} (last args, head args) : walk
+ "e" -> {-edges  -} make_pair args
+ "k" -> {-clique -} [(u, v) | u <- args, v <- args, u < v]
+ "w" -> {-walk   -} walk
+
+-- separate a list of vertices and edges from a list of vertex/edge data
+
+read_graph :: [Data] -> ( [Int], [(Int, Int)] )
+read_graph = let
+ rough_read [] = ([], [])
+ rough_read (datum : _data) = let
+  (_V, _E) = rough_read _data
+  in case datum of
+  Vertex_data v _ _ -> (v : _V, _E)
+  Edge_data generator args -> (_V, gen_edges generator args ++ _E)
+ nub2 (xs, ys) = (nub xs, nub ys)
+ in nub2 . rough_read
+
+-- read file, decode tikz and construct graph object
+
+from_file :: String -> IO (Graph, String)
+from_file file_name = let
+ parse_it file_name parser input = case parse parser file_name input of
+  Left er -> fail ("error at " ++ show er)
+  Right x -> return x
+ create _data = ((uncurry graph) (read_graph _data), concatMap show _data)
+ in readFile file_name
+ >>= parse_it file_name (fmap create (tokeniser >>= annotator))
 
 
-main = readFile "graphs/triangle" >>= parseTest (tokeniser >>= annotator)
+main = from_file "graphs/triangle" >>= print
